11358064e7850b4a7855b0abdea6b6153f2e92eb
Teemu Puukko <teemuki@gmail.com>
2020-03-10 19:41:11 +0200

    Removing public forlder from versioncontrol

--- public/styles-b583a22d35c14c3d94e6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"k+z5":function(n,w,o){}}]);
-//# sourceMappingURL=styles-b583a22d35c14c3d94e6.js.map
--- public/styles-ebe7eac4540f8715ac2b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"k+z5":function(n,w,o){}}]);
-//# sourceMappingURL=styles-ebe7eac4540f8715ac2b.js.map
